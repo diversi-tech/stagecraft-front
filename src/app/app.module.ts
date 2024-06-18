5e91f8858fd4aab71f2711f445ad9d663babb816
AyalaGiat <giat9917938@gmail.com>
2024-06-18 12:20:09 +0300

    cliet1

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

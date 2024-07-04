e14d1d245b5d591245db53b30909fa42939ef6bf
Tovi7139517 <t0527139517@gmail.com>
2024-07-04 14:51:49 +0300

    more changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { TaskFilesComponent } from './components/task-files/task-files.component';
+//import { UserCoursesComponent } from './components/user-courses/user-courses.component';
+const routes: Routes = [
+{path:"login",component:LoginComponent},
+{path:"courseList",component:CourseListComponent},
+{path:"feedback",component:FeedbackComponent},
+//{path:"progressBar",component:progressbarComponent},
+{path:"taskFiles",component:TaskFilesComponent},
+//{path:"userCourses",component:UserCoursesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

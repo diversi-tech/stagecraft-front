d20b7014875921b2fe26c7b5774204c4575ac278
Tehila <t0583202927>
2024-07-11 10:08:06 +0300

    login

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -2,27 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { users } from '../class/User';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private apiUrl = 'http://your-api-url'; // הזן את כתובת ה- API שלך כאן
+  private baseUrl: string = `${environment.baseUrl}/login/CheckUserExistence`; // הזן את כתובת ה- API שלך כאן
 
   constructor(private http: HttpClient) { }
-
+  
   // checkUserExistence(username: users): Observable<boolean> {
   //   return this.http.get<boolean>(`${this.apiUrl}/users/${username}/exists`);
   // }
 
   checkIfUserExists(user: users): Observable<boolean> {
-    const url = `${this.apiUrl}?email=${user.email}&password=${user.password}`;
-    return this.http.get<boolean>(url);
+    // const url = `${this.baseUrl}?email=${user.email}&password=${user.password}`;
+    return this.http.post<boolean>(`${this.baseUrl}`,user);
+  }
   }
 
-
-}
 
 
 

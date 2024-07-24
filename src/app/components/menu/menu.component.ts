6c3943265f53a123202970bf931ffe05d7a18412
batsheva <batsheva5634@gmail.com>
2024-07-24 08:07:11 +0300

    נגישות

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -7,18 +7,6 @@ import { UserService } from 'src/app/service/login.service';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-  accessibilityMode: boolean = false;
-constructor( private renderer: Renderer2,public userService: UserService){}
-
-toggleAccessibility() {
-  this.accessibilityMode = !this.accessibilityMode;
-  console.log('Accessibility mode:', this.accessibilityMode); // בדיקת מצב
-  if (this.accessibilityMode) {
-    this.renderer.addClass(document.body, 'accessibility-mode');
-  } else {
-    this.renderer.removeClass(document.body, 'accessibility-mode');
-  }
-  console.log('Class applied:', document.body.classList); // בדיקת מחלקות על body
-}
+constructor( public userService: UserService){}
 
 }
